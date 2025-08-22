99955f726e3c7bc93a9e1ca3dede3578ed27ab73
a-karlov <a-karlov@ASOFT21-PC001>
2025-08-22 13:46:20 +0300

    feat: add ioptions pattern

--- src/AdvertisingPlatforms.Infrastructure/DependencyInjection.cs
+++ src/AdvertisingPlatforms.Infrastructure/DependencyInjection.cs
@@ -1,14 +1,31 @@
 ﻿using AdvertisingPlatforms.Application.Interfaces;
 using AdvertisingPlatforms.Infrastructure.PlatformsTree;
+using AdvertisingPlatforms.Infrastructure.PlatformsTree.Options;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AdvertisingPlatforms.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services, 
+        IConfiguration configuration)
     {
-        services.AddSingleton<IPlatformTree, PlatformTree>();
+        services
+            .ConfigueOptions(configuration)
+            .AddSingleton<IPlatformTree, PlatformTree>();
+
+        return services;
+    }
+
+    public static IServiceCollection ConfigueOptions(
+        this IServiceCollection services, 
+        IConfiguration configuration)
+    {
+        services.AddOptions<PlatformTreeOptions>()
+            .Bind(configuration)
+            .ValidateOnStart();
 
         return services;
     }
---
+++ src/AdvertisingPlatforms.Infrastructure/PlatformsTree/Options/PlatformTreeOptions.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AdvertisingPlatforms.Infrastructure.PlatformsTree.Options;
+
+public class PlatformTreeOptions
+{
+    public const string SectionName = "PlatformTree";
+
+    [Required]
+    public int CacheCapacity { get; set; }
+}
